450d429a65b05a7b7932070ab5d302b6a216c92f
esmailin-25 <165542310+esmailin-25@users.noreply.github.com>
2026-04-16 19:51:28 -0400

    Update Final_practica.cpp

--- Final_practica.cpp
+++ Final_practica.cpp
@@ -3,46 +3,42 @@ using namespace std;
 
 int main() {
 
-
-   int n;
-   float notas[20][4];
-   float prom[20];
-   float suma, promGeneral = 0;
-   float mayor, menor;
-   int aprob = 0, reprob = 0;
-
+    int n;
+    float notas[20][4];
+    float prom[20];
+    float suma, promGeneral = 0;
+    float mayor, menor;
+    int aprob = 0, reprob = 0;
 
     // pedir cantidad de estudiantes
-   
-   do {
-        
-       cout << "Cantidad de estudiantes (max 20): ";
-       cin >> n;
-    
-   } while (n < 1 || n > 20);
+    do {
+        cout << "Cantidad de estudiantes (max 20): ";
+        cin >> n;
+    } while (n < 1 || n > 20);
 
-    
-   // ingresar notas
-   
-   for (int i = 0; i < n; i++) {
+    // ingresar notas
+    for (int i = 0; i < n; i++) {
         suma = 0;
         cout << "\nEstudiante " << i + 1 << endl;
 
-     for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < 4; j++) {
             do {
                 cout << "Nota " << j + 1 << ": ";
                 cin >> notas[i][j];
+
+                if (notas[i][j] < 0 || notas[i][j] > 100) {
+                    cout << "Error: la calificacion debe estar entre 0 y 100.\n";
+                }
+
             } while (notas[i][j] < 0 || notas[i][j] > 100);
 
             suma += notas[i][j];
 
-            
-            // inicializar mayor y menor con la primera nota real
+            // inicializar mayor y menor
             if (i == 0 && j == 0) {
                 mayor = notas[i][j];
                 menor = notas[i][j];
             }
-           
             else {
                 if (notas[i][j] > mayor) {
                     mayor = notas[i][j];
@@ -64,8 +60,7 @@ int main() {
         }
     }
 
-   
-   // mostrar resultados
+    // resultados
     cout << "\n--- RESULTADOS ---\n";
 
     for (int i = 0; i < n; i++) {
@@ -76,7 +71,6 @@ int main() {
         cout << "| Prom: " << prom[i] << endl;
     }
 
-   
     promGeneral = promGeneral / n;
 
     cout << "\nPromedio general: " << promGeneral << endl;
